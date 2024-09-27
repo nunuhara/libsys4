8d384af84c0c9e222e98689032f374ba4632a7c5
kichikuou <KichikuouChrome@gmail.com>
2024-09-27 18:01:45 +0900

    Add rmdir_utf8()
    This will be used to implement FileOperation.DeleteFolder in xsystem4.

--- include/system4/file.h
+++ include/system4/file.h
@@ -40,6 +40,7 @@ char *readdir_utf8(UDIR *dir);
 int stat_utf8(const char *path, ustat *st);
 char *realpath_utf8(const char *upath);
 int remove_utf8(const char *path);
+int rmdir_utf8(const char *path);
 FILE *file_open_utf8(const char *path, const char *mode);
 void *file_read(const char *path, size_t *len_out);
 bool file_write(const char *path, uint8_t *data, size_t data_size);
